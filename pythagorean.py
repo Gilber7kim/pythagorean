7aa37d9188222d6ebba2eb8ac46b9c4d158ac7b3
Gilbert Kim <gilbertkim@seoulchristian.org>
2016-01-24 21:42:15 +0900

    Fix the problems

--- pythagorean.py
+++ pythagorean.py
@@ -1,11 +1,70 @@
-#Pythagorean Theorem
-# a**2 + b**2 = c**2
-# This is a step to get data about the first and the second sides from the user. Command float is a way to insert a number as raw_input.
-a = float(raw_input("What is the length of the first side? -"))
-b = float(raw_input("What is the length of the second side? -"))
-#This is a process to define what c_squared is.
-c_squared = a**2 + b**2
-#Since the length of the hypotenuse is root a**2 + b**2, the expqonent (**) of the c_squared should be 0.5
-c = c_squared**0.5
-# This step shows the length of the hypotenuse.
-print "The length of the hypotenuse is %r" % c
+# define start
+def start():
+    # list what you wrote in the quotation marks.
+    print "Choose two sides to enter."
+    print "Don't forget that C is the hypotenuse!"
+    print "You can choose 1)AB 2)BC 3)AC"
+    print "Type in the number."
+    # the user can type the choices in
+    choice = raw_input("> ")
+
+    # These commands list all the possibilities the user can use.
+    if choice == "1":
+        AB()
+    elif choice == "2":
+        BC()
+    elif choice == "3":
+        AC()
+    else:
+        print "type 1, 2, or 3"
+
+
+# define AB
+def AB():
+    # float is the number form of raw_input
+    # so the user can type numbers instead of letters
+    A = float(raw_input("What is the length of A?\n> "))
+    B = float(raw_input("What is the length of B?\n> "))
+    # Define what C_squared is
+    C_squared = A**2 + B**2
+    # Define what C is
+    C = C_squared**0.5
+    # shows the answer
+    print "The length of the hypotenuse is %r" % C
+    # After all commands, the user goes out of the program automatically
+    exit(0)
+
+
+# define BC
+def BC():
+    # float is the number form of raw_input
+    # so the user can type numbers instead of letters
+    B = float(raw_input("What is the length of B?\n> "))
+    C = float(raw_input("What is the length of C?\n> "))
+    # Define what A_squared is
+    A_squared = C**2 - B**2
+    # Define what A is
+    A = A_squared**0.5
+    # shows the answer
+    print "The length of the A is %r" % A
+    # After all commands, the user goes out of the program automatically
+    exit(0)
+
+
+# define AC
+def AC():
+    # float is the number form of raw_input
+    # so the user can type numbers instead of letters
+    A = float(raw_input("What is the length of A?\n> "))
+    C = float(raw_input("What is the length of C?\n> "))
+    # Define what B_squared is
+    B_squared = C**2 - A**2
+    # Define what B is
+    B = B_squared**0.5
+    # shows the answer
+    print "The length of the B is %r" % B
+    # After all commands, the user goes out of the program automatically
+    exit(0)
+
+# It makes the start as the starting point of the whole program
+start()
